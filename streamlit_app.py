4f0e26b4b2a5d729343d64d16927df452dcfaec5
ngoldman10 <39044036+ngoldman10@users.noreply.github.com>
2022-12-04 15:01:55 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,12 @@ import math
 import pandas as pd
 import streamlit as st
 import requests
-import os
 import openai
 
 """
 This is an app
 """
 
-openai.api_key = os.getenv(API_KEY)
+openai.api_key = API_KEY
 mlist = openai.Model.list()
 st.json(mlist,expanded=True)
