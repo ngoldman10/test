5d7b53c1c2811bd7a9e9cb612eff86d968d03132
ngoldman10 <39044036+ngoldman10@users.noreply.github.com>
2022-12-04 14:19:23 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,10 @@ from sleeper_wrapper import League
 This is an app
 """
 
+league_id = '865065387836960768'
+league = League(league_id)
 
 title = st.text_input('Movie title', 'user input')
 st.write('Try this:', title)
+
+league.get_rosters()
