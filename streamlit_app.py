13a096eb492cae1755fb2577f0bae1079c02667c
ngoldman10 <39044036+ngoldman10@users.noreply.github.com>
2022-12-04 14:49:59 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,18 +4,12 @@ import math
 import pandas as pd
 import streamlit as st
 import requests
-from sleeper_wrapper import League
+import os
+import openai
 
 """
 This is an app
 """
 
 
-
-title = st.text_input('Movie title', 'user input')
-st.write('Try this:', title)
-
-league_id = '865065387836960768'
-league = League(league_id)
-a = league.get_rosters()
 st.json(a, expanded=True)
