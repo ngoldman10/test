74a4ad4945c164643d8a7a4da420e2df75ec960a
ngoldman10 <39044036+ngoldman10@users.noreply.github.com>
2022-12-04 18:54:25 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 import streamlit as st
 import openai
 
@@ -6,7 +7,7 @@ import openai
 This is an app
 """
 
-
+openai.api_key = st.secrets.key
 r = openai.Completion.create(
   model="text-davinci-003",
   prompt="Say this is a test",
