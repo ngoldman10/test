74ad2e568bf86ff0e5ae811564803d8eb4a335ba
ngoldman10 <39044036+ngoldman10@users.noreply.github.com>
2022-12-04 19:10:06 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,14 @@ import openai
 This is an app
 """
 
+prompt1 = st.text_input('Prompt', 'Tell me a joke')
+
 openai.api_key = st.secrets.key
 r = openai.Completion.create(
   model="text-davinci-003",
-  prompt="Say this is a test",
-  max_tokens=7,
-  temperature=0
+  prompt=prompt1,
+  max_tokens=50,
+  temperature=1
 )
 
 st.json(r,expanded=True)
