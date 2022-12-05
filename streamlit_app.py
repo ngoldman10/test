8c48775f1dfd50d86cadb13349fd160876438107
ngoldman10 <39044036+ngoldman10@users.noreply.github.com>
2022-12-04 18:04:04 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,11 @@ This is an app
 """
 key = ${{ secrets.SuperSecret }}
 openai.api_key = key
-openai.Completion.create(
+r = openai.Completion.create(
   model="text-davinci-003",
   prompt="Say this is a test",
   max_tokens=7,
   temperature=0
 )
+
+st.json(r,expanded=True)
