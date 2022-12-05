11cd14947740166d400872c9068dbbccc63b54a0
ngoldman10 <39044036+ngoldman10@users.noreply.github.com>
2022-12-04 17:43:07 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,16 @@
-from collections import namedtuple
-import altair as alt
-import math
 import pandas as pd
 import streamlit as st
-import requests
 import openai
 
 """
 This is an app
 """
+
+openai.api_key = "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"
+
+openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Say this is a test",
+  max_tokens=7,
+  temperature=0
+)
