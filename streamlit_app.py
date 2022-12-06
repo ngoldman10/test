edfd331279b4be7678bc42202ec3962d7e69bbea
ngoldman10 <39044036+ngoldman10@users.noreply.github.com>
2022-12-05 20:44:45 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,17 +12,16 @@ prompt1 = st.text_input('Prompt', initial_topic)
 
 combo = (prompt_start + prompt1 + prompt_end)
 
-openai.api_key = st.secrets.key
+#openai.api_key = st.secrets.key
 
-r = openai.Completion.create(
-  model="text-davinci-003",
-  prompt=combo,
-  max_tokens=256,
-  temperature=.9
-)
+#r = openai.Completion.create(
+#  model="text-davinci-003",
+#  prompt=combo,
+#  max_tokens=256,
+#  temperature=.9)
 
-rtext = r['choices'][0]['text']
-rtext = rtext.replace("\n","")
+#rtext = r['choices'][0]['text']
+#rtext = rtext.replace("\n","")
 
 st.write('')
 st.write('')
@@ -34,6 +33,7 @@ display = st.radio(
 if display == 'Still airing Get Up!':
     st.write("Don't touch that dial!")
 else:
-    st.write(rtext)
-    st.image("https://ih1.redbubble.net/image.2640223911.0420/st,small,507x507-pad,600x600,f8f8f8.jpg")
+    st.write("Good to go!")
+    #st.write(rtext)
+    #st.image("https://ih1.redbubble.net/image.2640223911.0420/st,small,507x507-pad,600x600,f8f8f8.jpg")
 
