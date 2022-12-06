03130f88c447511d18c337a9f82a6285cefd22ba
ngoldman10 <39044036+ngoldman10@users.noreply.github.com>
2022-12-05 16:03:52 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,4 +28,4 @@ st.write('')
 st.write('')
 st.write(rtext)
 
-st.image('https://ih1.redbubble.net/image.2640223911.0420/st,small,507x507-pad,600x600,f8f8f8.jpg')
+st.image("https://ih1.redbubble.net/image.2640223911.0420/st,small,507x507-pad,600x600,f8f8f8.jpg")
