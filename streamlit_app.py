e14705f1b96383ce9e26de3fa0405bdb7041413d
ngoldman10 <39044036+ngoldman10@users.noreply.github.com>
2022-12-05 21:43:42 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,46 +3,32 @@ import os
 import streamlit as st
 import openai
 
-
+#prompt beginning and ending to pass to API
 prompt_start = ("Speak with the tone and vernacular of Stephen A. Smith from ESPN.\n\nPremise: Someone gets Stephen A. Smith started on ")
 prompt_end = (", and Stephen A. Smith responds.\n\nStephen A. Smith:")
-#initial_topic = ("the best highways in America")
 
-
+#Variable to store text input
 prompt1 = st.text_input('Prompt', "Enter a topic then press enter. Ex:the best highways in America", key="textkey")
-
-st.write(st.session_state.textkey)
-
-if st.session_state.textkey == "Enter a topic then press enter. Ex:the best highways in America":
-    st.write("Have not begun")
-else:
-    st.write("We have indeed begun!")
-    st.write("This is prompt 1: "+ prompt1)
-
 combo = (prompt_start + prompt1 + prompt_end)
 
-#openai.api_key = st.secrets.key
-
-#r = openai.Completion.create(
-#  model="text-davinci-003",
-#  prompt=combo,
-#  max_tokens=256,
-#  temperature=.9)
-
-#rtext = r['choices'][0]['text']
-#rtext = rtext.replace("\n","")
-
 st.write('')
 st.write('')
 
-display = st.radio(
-    "Start the show",
-    ('Still airing Get Up!', 'First take is now Live'))
+#api key
+openai.api_key = st.secrets.key
 
-if display == 'Still airing Get Up!':
-    st.write("Don't touch that dial!")
+#main flow
+if st.session_state.textkey == "Enter a topic then press enter. Ex:the best highways in America":
+  st.write("I've got something to say when you press enter.")
 else:
-    st.write("Good to go!")
-    #st.write(rtext)
-    #st.image("https://ih1.redbubble.net/image.2640223911.0420/st,small,507x507-pad,600x600,f8f8f8.jpg")
+  st.write("This is prompt 1: "+ prompt1)
+  r = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=combo,
+    max_tokens=256,
+    temperature=.9)
+  rtext = r['choices'][0]['text']
+  rtext = rtext.replace("\n","")
+  st.write(rtext)
+  st.image("https://ih1.redbubble.net/image.2640223911.0420/st,small,507x507-pad,600x600,f8f8f8.jpg")
 
