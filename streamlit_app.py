e4a5ae87a6e4901d38cc4b654eec41fb6c42662d
ngoldman10 <39044036+ngoldman10@users.noreply.github.com>
2022-12-15 21:58:12 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ combo = (prompt_start + prompt1 + prompt_end)
 
 #count tokens
 tot_input_tokens = len(tokenizer(combo)['input_ids'])
-st.write(tot_input_tokens)
+st.write("Input tokens: ", tot_input_tokens)
 
 st.write('')
 st.write('')
@@ -34,6 +34,8 @@ else:
     temperature=.9)
   rtext = r['choices'][0]['text']
   rtext = rtext.replace("\n","")
+  tot_output_tokens = len(tokenizer(rtext)['input_ids'])
   st.write(rtext)
+  st.write("Output tokens: ", rtext)
   st.image("https://ih1.redbubble.net/image.2640223911.0420/st,small,507x507-pad,600x600,f8f8f8.jpg")
 
